de7db4c16138bc897725cd3521511aae41270408
pgangwar <pgangwar@AD.UCSD.EDU@raptor.ucsd.edu>
2023-04-28 18:16:27 -0700

    tbb::pipeline implemented for read placement

--- src/matUtils/experiment.hpp
+++ src/matUtils/experiment.hpp
@@ -45,22 +45,19 @@ struct parsimony {
 };
 
 struct min_parsimony {
-   std::vector<size_t> idx_list;
+   std::vector<int> idx_list;
    std::vector<std::vector<MAT::Mutation>> par_list;
-   std::vector<bool> is_sibling_list;
 };
 
 po::variables_map parse_extract_command(po::parsed_options parsed);
 void simulate_and_place_reads(po::parsed_options parsed);
 
-void read_vcf(std::string, std::vector<struct read_info*> &);
+void read_vcf(uint32_t, const MAT::Tree &, const std::vector<MAT::Node*> &, const std::string);
 
-struct score_read {
-   double score;
-   std::vector<size_t> reads;
-};
-void place_reads(const MAT::Tree &, const std::vector<MAT::Node*> &, const std::vector<struct read_info*> &, tbb::concurrent_hash_map<MAT::Node*, score_read> &, tbb::concurrent_hash_map<size_t, struct min_parsimony> &, std::string);
-void analyze_reads(const MAT::Tree &, const std::vector<MAT::Node*> &, const std::vector<struct read_info*> &, tbb::concurrent_hash_map<MAT::Node*, score_read> &, tbb::concurrent_hash_map<size_t, struct min_parsimony> &);
+std::vector<int> place_reads(const MAT::Tree &, const std::vector<MAT::Node*> &, struct read_info*, tbb::concurrent_hash_map<MAT::Node*, double> &);
+
+//void analyze_reads(const MAT::Tree &, const std::vector<MAT::Node*> &, const std::vector<struct read_info*> &, tbb::concurrent_hash_map<MAT::Node*, score_read> &, tbb::concurrent_hash_map<size_t, struct min_parsimony> &);
 
 size_t branch_distance(MAT::Node*, MAT::Node*);
 std::string get_clade(const MAT::Tree &, MAT::Node*);
+int get_overlap(struct read_info*, struct read_info*);
