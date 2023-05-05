364b5ce62626039aa58b641fc5b40b5d109ad307
pgangwar <pgangwar@AD.UCSD.EDU@raptor.ucsd.edu>
2023-05-05 11:32:52 -0700

    Clade Abundance updated

--- src/matUtils/experiment.hpp
+++ src/matUtils/experiment.hpp
@@ -52,11 +52,11 @@ struct min_parsimony {
 po::variables_map parse_extract_command(po::parsed_options parsed);
 void simulate_and_place_reads(po::parsed_options parsed);
 
-void read_vcf(uint32_t, const MAT::Tree &, const std::vector<MAT::Node*> &, std::unordered_map<int, struct read_info*> &, tbb::concurrent_hash_map<MAT::Node*, double> &, const std::string);
+void read_vcf(uint32_t, const MAT::Tree &, const std::vector<MAT::Node*> &, std::unordered_map<int, struct read_info*> &, const std::unordered_map<std::string, double> &, tbb::concurrent_hash_map<MAT::Node*, double> &, tbb::concurrent_hash_map<std::string, double> &, const std::string);
 
-int place_reads(const MAT::Tree &, const std::vector<MAT::Node*> &, struct read_info*, const MAT::Node*, const std::unordered_map<std::string, double> &, tbb::concurrent_hash_map<MAT::Node*, double> &, const int);
+int place_reads(const MAT::Tree &, const std::vector<MAT::Node*> &, struct read_info*, const MAT::Node*, const std::unordered_map<std::string, double> &, tbb::concurrent_hash_map<MAT::Node*, double> &, tbb::concurrent_hash_map<std::string, double> &, const bool, const int);
 
-void analyze_reads(const MAT::Tree &, const std::vector<MAT::Node*> &, const std::unordered_map<int, struct read_info*>&, tbb::concurrent_hash_map<MAT::Node*, double> &);
+void analyze_reads(const MAT::Tree &, const std::vector<MAT::Node*> &, const std::unordered_map<int, struct read_info*> &, std::unordered_map<std::string, double> &, tbb::concurrent_hash_map<MAT::Node*, double> &, tbb::concurrent_hash_map<std::string, double> &);
 
 size_t branch_distance(MAT::Node*, MAT::Node*);
 std::string get_clade(const MAT::Tree &, MAT::Node*);
