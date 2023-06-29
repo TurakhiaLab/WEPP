fb769fd1965c319869f2ac81b020883fe6eb8c87
pgangwar <pgangwar@AD.UCSD.EDU@raptor.ucsd.edu>
2023-06-28 18:01:46 -0700

    Algorithm updated

--- regression_abundance_estimate.py
+++ regression_abundance_estimate.py
@@ -7,14 +7,14 @@ def read_csv_file(file_path):
     with open(file_path, 'r') as file:
         csv_reader = csv.reader(file)
         mutations = next(csv_reader)  # Store headers separately
-        peak_nodes = []
+        haplotypes = []
         peak_mut_matrix = []
         
         for row in csv_reader:
-            peak_nodes.append(row[0])
+            haplotypes.append(row[0])
             peak_mut_matrix.append(row[1:])
     
-    return mutations, peak_nodes, peak_mut_matrix
+    return mutations, haplotypes, peak_mut_matrix
 
 
 def read_vcf_file(file_path):
@@ -29,10 +29,10 @@ def read_vcf_file(file_path):
     return af_values
 
 
-def write_vcf_file(file_path, mut_peak, peak_nodes, mutations):
+def write_vcf_file(file_path, mut_peak, haplotypes, mutations):
     with open(file_path, 'w') as file:
         file.write("##fileformat=VCFv4.2\n##reference=stdin:hCoV-19/Wuhan/Hu-1/2019|EPI_ISL_402125|2019-12-31\n")
-        file.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t{}\n".format("\t".join(peak_nodes)))
+        file.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t{}\n".format("\t".join(haplotypes)))
         i = 0
         while i < len(mutations):
             same_pos_list = []
@@ -55,35 +55,49 @@ def write_vcf_file(file_path, mut_peak, peak_nodes, mutations):
             i += 1 + len(same_pos_list)
 
 
-def add_mutations_to_haplotypes(A, sol, b): 
-    ref_cost = np.linalg.norm(A.astype('float64') @ sol - b, 1)
+def add_mutations_to_haplotypes(A, x_val, b): 
+    hap_indices = []
     A_copy = np.copy(A)
-    residual = abs(A_copy.astype('float64') @ sol - b)
-    sorted_sites = np.argsort(residual)[::-1] 
-    peak_indices = []
-    for site in sorted_sites:
-        peak_idx = -1
+    ref_cost = np.linalg.norm(A.astype('float64') @ x_val - b, 1)
+    sol = x_val
+    while True:
+        hap_idx = -1
+        residual = abs(A_copy.astype('float64') @ sol - b)
+        site = np.argmax(residual)
         for i in range(len(A_copy[site])):
             #Only add mutation to haplotype where it is absent
             if not int(A_copy[site, i]):
-                A_copy[site, i] = 1
-                curr_cost = np.linalg.norm(A_copy.astype('float64') @ sol - b, 1)
+                #Copy the column at the end of the array and add the mutation there
+                A_copy = np.concatenate((A_copy, A_copy[:, i][:, np.newaxis]), axis=1)
+                A_copy[site, -1] = 1
+                #Find the new value of x
+                x = cp.Variable(A_copy.shape[1])
+                cost = cp.norm(A_copy @ x - b, 1)
+                constraints = [sum(x) == 1, x >= 0]
+                prob = cp.Problem(cp.Minimize(cost), constraints)
+                prob.solve(verbose=False)
+                curr_cost = np.linalg.norm(A_copy.astype('float64') @ x.value - b, 1)
+                #Removing the last column that was added
+                A_copy = A_copy[:, :-1]
+                #Update the ref_cost
                 if (abs(curr_cost - ref_cost) > 1e-9) and (curr_cost < ref_cost):
                     ref_cost = copy.deepcopy(curr_cost)
-                    peak_idx = i
-                #Bring the original mutation back
-                A_copy[site, i] = 0
-        if peak_idx >= 0:
-            #Change the mutation in haplotype bringing the biggest change
-            A_copy[site, peak_idx] = 1
-            #Add unique peak indexes in the list
-            if peak_idx not in peak_indices:
-                peak_indices.append(peak_idx)
-    
-    return A_copy, peak_indices
+                    hap_idx = i
+                    sol = x.value
+        #Check if cost could be reduced 
+        if hap_idx >= 0:
+            #Add the hap_idx column at the end of A_copy
+            A_copy = np.concatenate((A_copy, A_copy[:, hap_idx][:, np.newaxis]), axis=1)
+            A_copy[site, -1] = 1
+            hap_indices.append(hap_idx)
+        else:
+            if A_copy.shape == A.shape:
+                print("No Haplotype added!!!")
+            break
+    return A_copy, hap_indices, np.abs(sol)
 
 
-def solve_abundance(peak_mut_matrix, read_values, peak_nodes, mutations):
+def solve_abundance(peak_mut_matrix, read_values, haplotypes, mutations):
     #Setting the variables for the regression problem
     eps = 1e-3
     A = np.array(np.array(peak_mut_matrix).T)
@@ -94,20 +108,28 @@ def solve_abundance(peak_mut_matrix, read_values, peak_nodes, mutations):
     prob = cp.Problem(cp.Minimize(cost), constraints)
     prob.solve(verbose=False)
     sol = x.value
+    
+    #Add new haplotype names
+    A_new, hap_idx_add, sol = add_mutations_to_haplotypes(A, np.abs(sol), b)
+    hap_new = copy.deepcopy(haplotypes)
+    count_dict = {}
+    for idx in hap_idx_add:
+        if idx in count_dict:
+            count_dict[idx] += 1
+        else:
+            count_dict[idx] = 1
+        hap_new += f'NEW_{count_dict[idx]}_{haplotypes[idx]}'
+    hap_new = [hap + "_READ_1_29902" for hap in hap_new]
+
     #Make the proportion to be 0 for anything < eps
     sol[sol < eps] = 0
-    #Removing peaks that are < 0 in abundance
-    peak_idx_remove = np.where(sol == 0)[0]
-    A_new = np.delete(A, peak_idx_remove, axis = 1)
-    peak_nodes_new = [val for i, val in enumerate(peak_nodes) if i not in peak_idx_remove]
-
-    #Adding peaks that were modified to reduce cost
-    A_hap_modified, peak_idx_add = add_mutations_to_haplotypes(A, sol, b)
-    A_new = np.concatenate((A_new, A_hap_modified[:, peak_idx_add]), axis = 1)
-    peak_nodes_new += ["NEW_" + peak_nodes[idx] for idx in peak_idx_add]
-    peak_nodes_new = [peak + "_READ_0_29902" for peak in peak_nodes_new]
-
-    #Remove rows with all zeros
+    #Removing haplotypes that are < 0 in abundance
+    hap_idx_remove = np.where(sol == 0)[0]
+    A_new = np.delete(A_new, hap_idx_remove, axis = 1)
+    hap_new = [val for i, val in enumerate(hap_new) if i not in hap_idx_remove]
+    sol = [val for i, val in enumerate(sol) if i not in hap_idx_remove]
+    
+    #Remove rows with all zeros (due to removal of haplotypes)
     A_new = A_new.astype(int)
     zero_rows = np.all(A_new == 0, axis=1)
     mut_idx_remove = np.where(zero_rows)[0]
@@ -116,23 +138,21 @@ def solve_abundance(peak_mut_matrix, read_values, peak_nodes, mutations):
 
     #Write VCF
     vcf_file = "my_vcf_haplotypes.vcf"
-    write_vcf_file(vcf_file, A_new, peak_nodes_new, mutations_new)
-
-    #if np.array_equal(A, A_hap_modified):
-    #    print("No New Haplotype found!!!")
-    return sol
+    write_vcf_file(vcf_file, A_new, hap_new, mutations_new)
+    
+    return hap_new, sol
 
 
 # Reading File
 barcode_file_path = 'my_vcf_barcode.csv'
-mutations, peak_nodes, peak_mut_matrix = read_csv_file(barcode_file_path)
+mutations, haplotypes, peak_mut_matrix = read_csv_file(barcode_file_path)
 mutations = mutations[1:]
 
 vcf_file = 'my_vcf_abundance.vcf'
 af_values = read_vcf_file(vcf_file)
 
 #Solving abundance
-abundances = solve_abundance(peak_mut_matrix, af_values, peak_nodes, mutations)
+haplotypes, abundances = solve_abundance(peak_mut_matrix, af_values, haplotypes, mutations)
 
-#for i in range(len(peak_nodes)):
-#    print(peak_nodes[i], abundances[i])
+for i in range(len(haplotypes)):
+    print(haplotypes[i], abundances[i])
