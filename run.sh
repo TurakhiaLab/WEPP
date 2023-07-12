cf9b47364dc4f043dfa67c3a21e8c574c6e0e200
pgangwar <pgangwar@AD.UCSD.EDU@raptor.ucsd.edu>
2023-07-11 18:02:48 -0700

    Threads added to post_processing

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@ export PATH=$PATH:$PWD/build
 cd build
 make -j
 cd ../
-matUtils place_read -T 48 -i public-2021-05-31.all.masked.nextclade.pangolin.pb -l B.1.160,B.1.177.7,B.1.429,P.1,B.42,R.1,B.33  -d 0.2,0.15,0.15,0.15,0.1,0.05,0.2 -v my_vcf -r 150 -w 20 -e 0 -s 100 -f test/NC_045512v2.fa
+matUtils place_read -T 48 -i public-2021-05-31.all.masked.nextclade.pangolin.pb -l B.1.160,B.1.177.7,B.1.429,P.1,B.42,R.1,B.33 -d 0.2,0.15,0.15,0.15,0.1,0.05,0.2 -v my_vcf -r 150 -w 20 -e 0 -s 100 -f test/NC_045512v2.fa
 
 #Estimating using regression based approach
 python regression_abundance_estimate.py
-matUtils post_processing -i public-2021-05-31.all.masked.nextclade.pangolin.pb -v my_vcf
+matUtils post_processing -T 48 -i public-2021-05-31.all.masked.nextclade.pangolin.pb -v my_vcf
 
 #Estimating using EM approach
 Rscript abundance.r 
@@ -17,3 +17,6 @@ Rscript abundance.r
 #source run.sh 
 #conda deactivate
 #cd -
+
+#usher -i public-2021-05-31.all.masked.nextclade.pangolin.pb -v my_vcf_haplotypes.vcf --no-add --write-parsimony-scores-per-node
+#awk -F'\t' '$5 == "y"' parsimony-scores.tsv 
