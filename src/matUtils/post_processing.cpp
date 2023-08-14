f31725f1dbcb3b0d49705dd1820e86e00b8859db
pgangwar <pgangwar@AD.UCSD.EDU@raptor.ucsd.edu>
2023-08-13 22:29:43 -0700

    Neighborhood peaks added

--- src/matUtils/post_processing.cpp
+++ src/matUtils/post_processing.cpp
@@ -1,6 +1,8 @@
 #include "post_processing.hpp"
 
 po::variables_map parse_post_processing_command(po::parsed_options parsed) {
+    uint32_t num_cores = tbb::task_scheduler_init::default_num_threads();
+    std::string num_threads_message = "Number of threads to use when possible [DEFAULT uses all available cores, " + std::to_string(num_cores) + " detected on this machine]";
     po::variables_map vm;
     po::options_description conv_desc("post_processing options");
     conv_desc.add_options()
@@ -10,6 +12,7 @@ po::variables_map parse_post_processing_command(po::parsed_options parsed) {
      "Write output files to the target directory. Default is current directory.")
     ("read-vcf,v", po::value<std::string>()->default_value(""),
      "Output VCF file representing selected subtree. Default is full tree")
+    ("threads,T", po::value<uint32_t>()->default_value(num_cores), num_threads_message.c_str())
     ("help,h", "Print help messages");
     // Collect all the unrecognized options from the first pass. This will include the
     // (positional) command name, so we need to erase that.
@@ -39,38 +42,190 @@ void post_processing(po::parsed_options parsed) {
     std::string dir_prefix = vm["output-directory"].as<std::string>();
     std::string sample_vcf_filename = dir_prefix + vm["read-vcf"].as<std::string>() + "_samples.vcf";
     std::string hap_vcf_filename = dir_prefix + vm["read-vcf"].as<std::string>() + "_haplotypes.vcf";
+    std::string read_vcf_filename = dir_prefix + vm["read-vcf"].as<std::string>() + "_reads.vcf";
     std::string hap_csv_filename = dir_prefix + vm["read-vcf"].as<std::string>() + "_hap_abundance.csv";
     std::string hap_clade_csv_filename = dir_prefix + vm["read-vcf"].as<std::string>() + "_hap_clade.csv";
+    uint32_t num_threads = vm["threads"].as<uint32_t>();
 
+    //Initialize TBB threads
+    tbb::task_scheduler_init init(num_threads);
+    
     // Load input MAT and uncondense tree
     MAT::Tree T;
     T = MAT::load_mutation_annotated_tree(input_mat_filename);
     T.uncondense_leaves();
 
     //Checking how close are input samples with peaks
-    std::unordered_map<int, struct read_info*> read_map;
+    std::unordered_map<int, struct read_info*> read_map, hap_map;
+    tbb::concurrent_hash_map<std::string, std::vector<size_t>> hap_read_map;
+    std::unordered_map<std::string, std::string> hap_clade_map;
     std::vector<std::string> vcf_samples;
     std::unordered_map<std::string, double> hap_abun_map;
-    std::unordered_map<std::string, std::string> hap_clade_map;
     read_sample_vcf(vcf_samples, sample_vcf_filename);
-    read_vcf(read_map, hap_vcf_filename);
+    read_vcf(hap_map, hap_vcf_filename);
     read_csv(hap_abun_map, hap_csv_filename);
     read_csv(hap_clade_map, hap_clade_csv_filename);
+    
+    //read_vcf(read_map, read_vcf_filename);
+    //place_reads(read_map, hap_map, hap_abun_map, hap_read_map);
+    
     compute_abundance(hap_abun_map, hap_clade_map);
-    compute_distance(T, read_map, vcf_samples);
+    compute_distance(T, hap_map, vcf_samples);
+
 }
 
 
-void compute_distance(const MAT::Tree &T, const std::unordered_map<int, struct read_info*> &read_map, const std::vector<std::string> &vcf_samples) {
-    fprintf(stderr, "Haplotypes: %d\n\n", (int)read_map.size());
+void place_reads(const std::unordered_map<int, struct read_info*>& read_map, const std::unordered_map<int, struct read_info*>& hap_map, const std::unordered_map<std::string, double>& hap_abun_map, tbb::concurrent_hash_map<std::string, std::vector<size_t>>& hap_read_map) {
+    //Calculating node score for remaining reads
+    static tbb::affinity_partitioner ap;
+    tbb::parallel_for( tbb::blocked_range<size_t>(0, read_map.size()),
+        [&](tbb::blocked_range<size_t> k) {
+            for (size_t i = k.begin(); i < k.end(); ++i) {
+                //Find the most parsimonious positions
+                auto read_id = read_map.find(i)->second;
+                std::vector<std::string> epp_list;
+                int min_dist = std::numeric_limits<int>::max();
+                auto hap_itr = hap_map.begin();
+                while (hap_itr != hap_map.end()) {
+                    std::vector<MAT::Mutation> mutations_in_range;
+                    for (auto m: hap_itr->second->mutations)
+                        if ((m.position >= read_id->start) && (m.position <= read_id->end))
+                            mutations_in_range.emplace_back(m);
+                    int curr_dist = mutation_distance(read_id->mutations, mutations_in_range);
+                    mutations_in_range.clear();
+                    if (curr_dist <= min_dist) {
+                        if (curr_dist < min_dist) {
+                            epp_list.clear();
+                            min_dist = curr_dist;
+                        }
+                        epp_list.emplace_back(hap_itr->second->read);
+                    }
+                    hap_itr++;
+                }
+                //Get probabilities of all the EPP entries
+                std::vector<double> prob_list;
+                double total_prob = 0.0;
+                for (auto epp: epp_list) {
+                    double prob = hap_abun_map.find(epp)->second;
+                    prob_list.emplace_back(prob);
+                    total_prob += prob;
+                }
+
+                // Create a random number generator
+                std::random_device rd;
+                std::mt19937 gen(rd());
+                std::uniform_real_distribution<> dis(0.0, 1.0);
+                // Generate a random number between 0 and 1
+                double randomValue = dis(gen);
+                // Assign the 'read' variable based on probabilities
+                double cumulativeProbability = 0.0;
+                int selectedIdx = -1;
+                for (size_t j = 0; j < prob_list.size(); ++j) {
+                    cumulativeProbability += (prob_list[j] / total_prob);
+                    if (randomValue < cumulativeProbability) {
+                        selectedIdx = j;
+                        break;
+                    }
+                }
+
+                //Assign Read -> Haplotype
+                std::string haplotype = epp_list[selectedIdx]; 
+                std::vector<size_t> new_vec = {i};
+                tbb::concurrent_hash_map<std::string, std::vector<size_t>>::accessor ac;
+                auto created = hap_read_map.insert(ac, std::make_pair(haplotype, new_vec));
+                if (!created)
+                    ac->second.emplace_back(i);
+                ac.release();
+            }
+        },
+    ap);
+    
+    //Check mutation region of each haplotype
+    auto hrm_itr = hap_read_map.begin();
+    while (hrm_itr != hap_read_map.end()) {
+        std::unordered_map<int, std::vector<std::unordered_map<int8_t, int>>> read_mut_map;
+        for (auto r_idx: hrm_itr->second) {
+            for (auto mut: read_map.find(r_idx)->second->mutations) {
+                //Mutation seen at this position
+                if (read_mut_map.find(mut.position) != read_mut_map.end()) {
+                    auto& rm_vec = read_mut_map[mut.position];
+                    bool found = false;
+                    for (auto& rm_map: rm_vec) {
+                        // Increment the mutation count if found
+                        if (rm_map.find(mut.mut_nuc) != rm_map.end()) {
+                            rm_map[mut.mut_nuc]++;
+                            found = true;
+                            break;
+                        }
+                    }
+                    // Add new mutation to the list if not found
+                    if (!found) {
+                        std::unordered_map<int8_t, int> mut_count = {{mut.mut_nuc, 1}};
+                        rm_vec.emplace_back(mut_count);
+                    }
+                    
+                }
+                //Mutation not present in map
+                else {
+                    std::unordered_map<int8_t, int> mut_count = {{mut.mut_nuc, 1}};
+                    std::vector<std::unordered_map<int8_t, int>> mut_count_list = {mut_count};
+                    read_mut_map[mut.position] = mut_count_list;
+                }
+            }
+        }
+
+        //Get Haplotype mutations
+        std::vector<MAT::Mutation> hap_mut;
+        for (const auto& h_m: hap_map) {
+            if (h_m.second->read == hrm_itr->first) {
+                hap_mut = h_m.second->mutations;
+                break;
+            }
+        }
+        printf("\n\nHAP: %s, Total_reads: %d, hap_muts: %d\n", hrm_itr->first.c_str(), (int)hrm_itr->second.size(), (int)hap_mut.size());
+        for (auto mut: hap_mut)
+            printf(" %d%c", mut.position, MAT::get_nuc(mut.mut_nuc));
+
+        //Print mutations
+        for (const auto& r_mut: read_mut_map) {
+            //Count all reads that have this position
+            int total_reads = 0;
+            for (const auto r_idx: hrm_itr->second){
+                if ((r_mut.first >= read_map.find(r_idx)->second->start) && (r_mut.first <= read_map.find(r_idx)->second->end))
+                    total_reads++;
+            }
+            //Get the mutations present tat this position
+            for (const auto& allele_freq_list: r_mut.second) {
+                for (const auto& allele_freq: allele_freq_list) {
+                    bool present = false;
+                    for (auto m: hap_mut)
+                        if ((m.position == r_mut.first) && (m.mut_nuc == allele_freq.first)) {
+                            present = true;
+                            break;
+                        } 
+                    if (present)
+                        printf("\n%d%c -> %f PRESENT", r_mut.first, MAT::get_nuc(allele_freq.first), ((double)allele_freq.second / (double)total_reads));
+                    else
+                        printf("\n%d%c -> %f ABSENT", r_mut.first, MAT::get_nuc(allele_freq.first), ((double)allele_freq.second / (double)total_reads));
+                }
+            }
+        }
+        hrm_itr++;
+        read_mut_map.clear();
+    }
+}
+
+
+void compute_distance(const MAT::Tree &T, const std::unordered_map<int, struct read_info*> &hap_map, const std::vector<std::string> &vcf_samples) {
+    fprintf(stderr, "Haplotypes: %d\n\n", (int)hap_map.size());
     printf("\nMUTATION DISTANCE NEW:\n");
     //Get Mutations of samples
     for (auto sample: vcf_samples) {
         int min_dist = 100000;
         auto sample_mutations = get_mutations(T, sample);
         std::string best_node = "";
-        auto itr = read_map.begin();
-        while (itr != read_map.end()) {
+        auto itr = hap_map.begin();
+        while (itr != hap_map.end()) {
             int curr_dist = mutation_distance(sample_mutations, itr->second->mutations);
             if (curr_dist < min_dist) {
                 min_dist = curr_dist;
@@ -79,6 +234,7 @@ void compute_distance(const MAT::Tree &T, const std::unordered_map<int, struct r
             itr++;
         }
         printf("Node: %s, Closest_node: %s, mutation_distance: %d\n", sample.c_str(), best_node.c_str(), min_dist);
+        
     }
 
     //Depth first expansion to get all nodes in the tree and 
