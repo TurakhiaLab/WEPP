73f02053cb49401c773f56f002604994d832aeeb
pgangwar <pgangwar@AD.UCSD.EDU@raptor.ucsd.edu>
2023-09-17 11:31:03 -0700

    Modified code and update neighborhhod peak addition

--- src/matUtils/experiment.hpp
+++ src/matUtils/experiment.hpp
@@ -8,6 +8,9 @@
 #include <iostream>
 #include <boost/iostreams/filtering_stream.hpp>
 
+// Define your mutex type
+using my_mutex_t = tbb::queuing_mutex;
+
 struct ances_sample_list {
    Mutation_Annotated_Tree::Node * ancestor_node; 
    std::vector<Mutation_Annotated_Tree::Node*> * sample_nodes;
@@ -51,21 +54,31 @@ struct min_parsimony {
    std::vector<std::vector<MAT::Mutation>> par_list;
 };
 
+struct node_branch {
+   MAT::Node* node;
+   int branch_length;
+};
+
 po::variables_map parse_extract_command(po::parsed_options parsed);
 void simulate_and_place_reads(po::parsed_options parsed);
 
+
 void read_sample_vcf(std::vector<std::string> &, const std::string);
 
-void read_vcf(std::unordered_map<int, struct read_info*> &, const std::string);
+void read_vcf(std::unordered_map<size_t, struct read_info*> &, const std::string);
 
-int place_reads(const MAT::Tree &, const std::vector<MAT::Node*> &, struct read_info*, const MAT::Node*, tbb::concurrent_hash_map<MAT::Node*, double> &, const int);
+int place_reads(const std::vector<MAT::Node*> &, struct read_info*, const MAT::Node*, tbb::concurrent_hash_map<MAT::Node*, double> &);
 
-void analyze_reads(const MAT::Tree &, const MAT::Tree &, const std::vector<MAT::Node*> &, const std::unordered_map<int, struct read_info*> &, tbb::concurrent_hash_map<MAT::Node*, double> &, const std::vector<std::string> &, const std::string &, const std::string &, const std::string &);
+void analyze_reads(const MAT::Tree &, const MAT::Tree &, const std::unordered_map<size_t, struct read_info*> &, tbb::concurrent_hash_map<MAT::Node*, double> &, const std::vector<std::string> &, const std::string &, const std::string &);
 
 int mutation_distance(const MAT::Tree &, const MAT::Tree &, const MAT::Node*, const MAT::Node*);
+
 std::string get_clade(const MAT::Tree &, MAT::Node*);
 
 void add_neighbor_peaks(const MAT::Tree &, std::vector<MAT::Node*> &, const int, const int);
 
-void generate_regression_abundance_data(const MAT::Tree &, const std::vector<MAT::Node*> &, const std::unordered_map<int, struct read_info*> &, const std::string &, const std::string &);
+int child_nodes_addition(const MAT::Tree &, my_mutex_t &, const std::vector<MAT::Node*> &, std::vector<MAT::Node*> &, int &, std::vector<MAT::Node*> &, MAT::Node*, MAT::Node*, const int &, const int &, const int &, const int &);
+
+void generate_regression_abundance_data(const MAT::Tree &, const std::vector<MAT::Node*> &, const std::unordered_map<size_t, struct read_info*> &, const std::string &, const std::string &);
+
 bool compare_mutations(const MAT::Mutation &, const MAT::Mutation &);
