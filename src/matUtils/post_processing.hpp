1cbbce5e44572c21d8447742c930b70c7ecc9c44
pgangwar <pgangwar@AD.UCSD.EDU@raptor.ucsd.edu>
2023-09-19 17:24:16 -0700

    Fixed variable type in post_processing file

--- src/matUtils/post_processing.hpp
+++ src/matUtils/post_processing.hpp
@@ -10,7 +10,7 @@ void read_csv(std::unordered_map<std::string, std::string> &, const std::string)
 
 int mutation_distance(std::vector<MAT::Mutation>, std::vector<MAT::Mutation>);
 
-void compute_distance(const MAT::Tree &, const std::unordered_map<int, struct read_info*> &, const std::vector<std::string> &);
+void compute_distance(const MAT::Tree &, const std::unordered_map<size_t, struct read_info*> &, const std::vector<std::string> &);
 
 void compute_abundance(const std::unordered_map<std::string, double> &, const std::unordered_map<std::string, std::string> &);
 
