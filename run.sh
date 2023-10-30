9adec667d93cf0e0dc897de3723114f92c86630e
Pranav Gangwar <pgangwar@ucsd.edu>
2023-10-29 20:09:38 -0700

    Haplotype VCF generation added

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ cd build
 make -j
 cd ../
 
+#wbe selectHaplotypes -i public-2023-08-17.all.masked.nextclade.pangolin.pb -l AY.100,B.1.1.529,B.1.2,B.1.526,B.1.582,BA.1.18,XBB.1.5,XBB.1.9.1,P.1 -d 0.1,0.1,0.15,0.15,0.2,0.05,0.1,0.05,0.1 -v my_vcf -w 20
+
 wbe filterLineages -T 48 -i public-2021-05-31.all.masked.nextclade.pangolin.pb -v my_vcf -f test/NC_045512v2.fa
 echo -e "\nFREYJA - LINEAGE FILTER"
 python src/WBE/peaks_filtering.py my_vcf
@@ -10,13 +12,3 @@ wbe detectPeaks -T 48 -i public-2021-05-31.all.masked.nextclade.pangolin.pb -v m
 echo -e "\nFREYJA - PEAKS FILTER"
 python src/WBE/peaks_filtering.py my_vcf
 wbe refinePeaks -T 48 -i public-2021-05-31.all.masked.nextclade.pangolin.pb -v my_vcf -f test/NC_045512v2.fa
-
-###matUtils haplotype_pruning -i public-2021-05-31.all.masked.nextclade.pangolin.pb -w public-2023-08-17.all.masked.nextclade.pangolin_pruned.pb -s remove_sample.txt -d 5
-#
-###Estimating using regression based approach
-#####Getting the clades from haplotypes using USHeR
-#####usher -i public-2023-08-17.all.masked.nextclade.pangolin_pruned.pb -v my_vcf_haplotypes.vcf --no-add
-####usher -i public-2021-05-31.all.masked.nextclade.pangolin.pb -v my_vcf_haplotypes.vcf --no-add
-####echo "Haplotype,Clade" > my_vcf_hap_clade.csv
-####awk -F'\t' '{print $1 "," $NF}' clades.txt >> my_vcf_hap_clade.csv
-##
