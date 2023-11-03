26ac909b5d609cfd5b73e2cf596f4e2c6a8f3862
Pranav Gangwar <pgangwar@ucsd.edu>
2023-11-02 17:35:02 -0700

    Fixed DUMMY-OG getting selected as peak

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ cd build
 make -j
 cd ../
 
-#wbe selectHaplotypes -i public-2023-08-17.all.masked.nextclade.pangolin.pb -l AY.100,B.1.1.529,B.1.2,B.1.526,B.1.582,BA.1.18,XBB.1.5,XBB.1.9.1,P.1 -d 0.1,0.1,0.15,0.15,0.2,0.05,0.1,0.05,0.1 -v my_vcf -w 20
+###wbe selectHaplotypes -i public-2023-08-17.all.masked.nextclade.pangolin.pb -l AY.100,B.1.1.529,B.1.2,B.1.526,B.1.582,BA.1.18,XBB.1.5,XBB.1.9.1,P.1 -d 0.1,0.1,0.15,0.15,0.2,0.05,0.1,0.05,0.1 -v my_vcf -w 20
 
 wbe filterLineages -T 48 -i public-2021-05-31.all.masked.nextclade.pangolin.pb -v my_vcf -f test/NC_045512v2.fa
 echo -e "\nFREYJA - LINEAGE FILTER"
