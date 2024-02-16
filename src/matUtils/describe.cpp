5f763fd81de5670613a1e6c6b37de7ba6b6e62f7
pgangwar <pgangwar@AD.UCSD.EDU@peregrine.ucsd.edu>
2024-02-15 19:38:05 -0800

    mutation_paths changed to only give actual mutations from leaf nodes

--- src/matUtils/describe.cpp
+++ src/matUtils/describe.cpp
@@ -31,30 +31,67 @@ std::vector<std::string> mutation_paths(MAT::Tree* T, std::vector<std::string> s
 std::vector<std::string> mutation_paths_all(MAT:: Tree*T) {
     std::vector<std::string> mpaths;
     auto dfs = T->depth_first_expansion();
-    for (auto n: dfs) {
-        std::string cpath = n->identifier + "\t";
-        auto root_to_sample = T->rsearch(n->identifier, true);
-        std::reverse(root_to_sample.begin(), root_to_sample.end());
-        bool first_mut_found = false;
-        for (size_t j = 0; j < root_to_sample.size(); j++) {
-            auto n2 = root_to_sample[j];
-            //Handling special characters
-            if (!n2->mutations.empty()) {
-                if (!first_mut_found)
-                    first_mut_found = true;
-                else 
-                    cpath += ">";
-            }
-
-            //Iterating through n2 mutations
-            for (size_t i=0; i < n2->mutations.size(); i++) {
-                cpath += n2->mutations[i].get_string();
-                if (i+1 < n2->mutations.size()) {
-                    cpath += ",";
+    for (const auto& curr_node: dfs) {
+        if (curr_node->is_leaf()) {
+            std::string cpath = curr_node->identifier + "\t";
+            std::vector<MAT::Mutation> mut_list;
+            for (auto anc: T->rsearch(curr_node->identifier, true)) {
+                for (auto c_mut: anc->mutations) {
+                    //Only consider the mutation closest to root at a site
+                    auto m_itr = mut_list.begin();
+                    while (m_itr != mut_list.end()) {
+                        if (m_itr->position == c_mut.position)
+                            break;
+                        m_itr++;
+                    }
+                    if (m_itr == mut_list.end())
+                        mut_list.emplace_back(c_mut);
                 }
             }
+            //Remove mutations where ref_nuc is same as mut_nuc -> Back Mutation
+            auto m_itr = mut_list.begin();
+            while (m_itr != mut_list.end()) {
+                if (m_itr->mut_nuc == m_itr->ref_nuc)
+                    m_itr = mut_list.erase(m_itr);
+                else
+                    m_itr++;
+            }
+
+            //Reversing mutations to get from root to leaf    
+            std::vector<MAT::Node*> ancestor_list = T->rsearch(curr_node->identifier, true);
+            MAT::Node* prev_node = NULL;
+            int j = ancestor_list.size() - 1;
+            for (int i = mut_list.size() - 1; i >= 0; --i) {
+                auto curr_mut = mut_list[i];
+                bool found = false;
+                while (j >= 0) {
+                    MAT::Node* curr_node = ancestor_list[j];
+                    for (const auto& mut: curr_node->mutations) {
+                        if ((mut.position == curr_mut.position) && (mut.mut_nuc == curr_mut.mut_nuc)) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found) {
+                        if (prev_node == curr_node) {
+                            cpath += ",";
+                            cpath += MAT::get_nuc(curr_mut.ref_nuc) + std::to_string(curr_mut.position) + MAT::get_nuc(curr_mut.mut_nuc);
+                           
+                        }
+                        else {
+                            if (prev_node != NULL)
+                                cpath += ">";
+                            cpath += MAT::get_nuc(curr_mut.ref_nuc) + std::to_string(curr_mut.position) + MAT::get_nuc(curr_mut.mut_nuc);
+                         
+                        }
+                        prev_node = curr_node;
+                        break;
+                    }
+                    j--;
+                }
+            }
+            mpaths.emplace_back(cpath);
         }
-        mpaths.push_back(cpath);
     }
     return mpaths;
 }
