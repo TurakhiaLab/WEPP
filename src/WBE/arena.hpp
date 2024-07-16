419669c20df12ff9afc4307b14ca61f906b3381e
enigmurl <43832426+enigmurl@users.noreply.github.com>
2024-07-15 17:05:59 -0700

    Fix read2node

--- src/WBE/arena.hpp
+++ src/WBE/arena.hpp
@@ -525,7 +525,7 @@ public:
         }
 
         //Run Python script to generate sam files
-        std::string command = "python src/WBE/sam_generation.py " + std::string(dir_prefix) + " " + vm["output-files-prefix"].as<std::string>();
+        std::string command = "python src/WBE/sam_generation.py " + ds.directory() + " " + ds.file_prefix();
         int result = std::system(command.c_str());
         if (result)
             fprintf(stderr, "\nCannot run sam_generation.py\n");
