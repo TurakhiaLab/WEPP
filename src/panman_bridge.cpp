76ea3490f318be76115747893f4cf769498b9554
pgangwar <pgangwar@AD.UCSD.EDU@raptor-2.ucsd.edu>
2024-10-23 13:39:38 -0700

    fasta sequences are correct

--- src/panman_bridge.cpp
+++ src/panman_bridge.cpp
@@ -1,22 +1,11 @@
 #include "panman_bridge.hpp"
 
 coord_converter::coord_converter(const panmanUtils::Tree& t) {
-    const panmanUtils::BlockGapList &blockGaps = t.blockGaps;
     const std::vector<panmanUtils::Block> &blocks = t.blocks;
     const std::vector<panmanUtils::GapList> &gaps = t.gaps;
 
-    map.resize(blocks.size() + 1);
-    // Assigning block gaps
-    for (size_t i = 0; i < blockGaps.blockPosition.size(); i++)
-    {
-        map[blockGaps.blockPosition[i]].second.resize(blockGaps.blockGapLength[i]);
-    }
-
     std::string consensus = "";
-
-    // map[i].first = nuc_pos
-    // map[i].first
-
+    map.resize(blocks.size() + 1);
     int32_t maxBlockId = 0;
     for (size_t block_id = 0; block_id < blocks.size(); block_id++)
     {
@@ -34,17 +23,19 @@ coord_converter::coord_converter(const panmanUtils::Tree& t) {
                     break;
                 }
                 consensus += panmanUtils::getNucleotideFromCode(nucCode);
-                map[blockId].first.push_back({0, {}});
+                map[blockId].push_back({0, {}});
             }
             if (endFlag)
             {
                 break;
             }
         }
-        map[blockId].first.push_back({0, {}});
-        consensus += '_';
+        // Added to handle pangraph based panmat format
+        //map[blockId].push_back({0, {}});
+        //consensus += '_';
     }
     map.resize(maxBlockId + 1);
+    
     // Assigning nucleotide gaps
     for (size_t i = 0; i < gaps.size(); i++)
     {
@@ -53,39 +44,25 @@ coord_converter::coord_converter(const panmanUtils::Tree& t) {
         {
             int len = gaps[i].nucGapLength[j];
             int pos = gaps[i].nucPosition[j];
-            map[blockId].first[pos].second.resize(len, 0);
+            map[blockId][pos].second.resize(len, 0);
         }
     }
+    
     // Assigning coordinates
     int ctr = 1;
     int consensus_ctr = 0;
     for (size_t block_id = 0; block_id < map.size(); block_id++)
     {
-        for (size_t j = 0; j < map[block_id].second.size(); j++)
+        for (size_t j = 0; j < map[block_id].size(); j++)
         {
-            for (size_t k = 0; k < map[block_id].second[j].size(); k++)
+            for (size_t k = 0; k < map[block_id][j].second.size(); k++)
             {
-                for (size_t w = 0; w < map[block_id].second[j][k].second.size(); w++)
-                {
-                    map[block_id].second[j][k].second[w] = ctr;
-                    ctr++;
-                    reference += '_';
-                }
-                map[block_id].second[j][k].first = ctr;
-                ctr++;
-                reference += '_';
-            }
-        }
-        for (size_t j = 0; j < map[block_id].first.size(); j++)
-        {
-            for (size_t k = 0; k < map[block_id].first[j].second.size(); k++)
-            {
-                map[block_id].first[j].second[k] = ctr;
+                map[block_id][j].second[k] = ctr;
                 ctr++;
 
                 reference += '_';
             }
-            map[block_id].first[j].first = ctr;
+            map[block_id][j].first = ctr;
             ctr++;
 
             reference += consensus[consensus_ctr++];
--- src/panman_bridge.hpp
+++ src/panman_bridge.hpp
@@ -5,8 +5,9 @@
 
 #include "panman/panmanUtils.hpp"
 
-// https://github.com/amkram/panmap/blob/main/src/tree.hpp#L37
-typedef std::vector<std::pair<std::vector<std::pair<int,std::vector<int>>>, std::vector<std::vector<std::pair<int,std::vector<int>>>>>> globalCoords_t;
+// https://github.com/amkram/panmap/blob/main/src/seed_annotated_tree.cpp#L511
+// reference is pseudo-root initially, which is later converted to root
+typedef std::vector<std::vector<std::pair<int,std::vector<int>>>> globalCoords_t;
 
 class coord_converter {
     globalCoords_t map;
@@ -15,18 +16,10 @@ public:
 
     coord_converter(const panmanUtils::Tree& t);
 
-    std::pair<size_t, size_t> block_range(int blockId) const {
-        const auto& block = map[blockId];
-
-        size_t start = static_cast<size_t> (block.first.front().second.empty() ? block.first.front().first : block.first.front().second.front());
-        size_t end = static_cast<size_t>(block.first.back().first);
-        return std::make_pair(start, end);
-    }
-    
     size_t query(const int blockId, const int nucPosition, const int nucGapPosition) const {
         if(nucGapPosition == -1){
-            return map[blockId].first[nucPosition].first;
+            return map[blockId][nucPosition].first;
         }
-        return map[blockId].first[nucPosition].second[nucGapPosition];
+        return map[blockId][nucPosition].second[nucGapPosition];
     }
 };
