4127b5e55d09b9fc91d59c9a119f9e8d3fe075af
pgangwar <pgangwar@AD.UCSD.EDU@raptor-2.ucsd.edu>
2024-11-07 17:08:37 -0800

    Updated run.sh

--- run.sh
+++ run.sh
@@ -8,26 +8,20 @@ cd build
 make -j
 cd ../
 
-##Variables
-#file_path="point_loma_jan_9_2022_q1_trim"
-#file_prefix="PL_Jan_9_2022"
-#MAT="gisaidAndPublic.2022-01-15.masked.pb.gz"
-##MAT="gisaidAndPublic.2022-02-15.masked.pb.gz"
+#Variables
+file_path="houston_as_nov_29_2021"
+file_prefix="H_as_nov_29"
+#MAT="updated_gisaidAndPublic.2022-05-15.masked.pb"
+MAT="gisaidAndPublic.2021-11-15.masked.pb.gz"
 #REF_MAT="gisaidAndPublic.2022-05-01.masked.pb.gz"
 
-
-file_prefix="PL_Jan_9_2022"
-file_prefix_cmp="PL_Feb_7_2022"
-MAT="gisaidAndPublic.2022-01-15.masked.pb.gz"
-file_path="/data/pgangwar/point_loma_data/2022/point_loma_jan_9_2022_q1_trim"
-file_path_cmp="/data/pgangwar/point_loma_data/2022/point_loma_feb_7_2022_q1_trim"
-wbe analyzePeaks -T 1 -i ${MAT} -r ${MAT} -v ${file_prefix} -w ${file_prefix_cmp} -o ${file_path} -p ${file_path_cmp} -f NC_045512v2.fa 
+#cp ${file_path}/${file_prefix}_alignment.sam ../Freyja/${file_prefix}_alignment.sam
+#cd ../Freyja
+#source run.sh
+#cd -
 
 
-#file_path="houston_ne_nov_29_2021"
-#file_prefix="H_ne_nov_29"
-#MAT="gisaidAndPublic.2021-11-15.masked.pb.gz"
-#REF_MAT="gisaidAndPublic.2022-03-15.masked.pb.gz"
+#wbe analyzePeaks -T 1 -i ${MAT} -r ${MAT} -v ${file_prefix} -w ${file_prefix_cmp} -o ${file_path} -p ${file_path_cmp} -f NC_045512v2.fa 
 
 #### Simulating reads from SWAMPy
 ##conda activate SWAMPy
@@ -37,7 +31,7 @@ wbe analyzePeaks -T 1 -i ${MAT} -r ${MAT} -v ${file_prefix} -w ${file_prefix_cmp
 ##mv ${file_path}/${file_prefix}_R1.fastq ${file_path}/${file_prefix}_R1.orig_fastq
 ##mv ${file_path}/${file_prefix}_R2.fastq ${file_path}/${file_prefix}_R2.orig_fastq
 ##gzip ${file_path}/${file_prefix}_R1+R2.fastq
-#
+
 ## Running C-WAP
 #fastq_path=$PWD/$file_path
 #cd ../C-WAP
@@ -47,16 +41,28 @@ wbe analyzePeaks -T 1 -i ${MAT} -r ${MAT} -v ${file_prefix} -w ${file_prefix_cmp
 #cd ../Freyja
 #source run.sh
 #
-### Run WEPP Pipeline
+## Run WEPP Pipeline
 #cd ../SARS2-WBE
 #samtools fastq ${file_path}/resorted.bam > ${file_path}/${file_prefix}_reads.fastq
 #
 #source src/WBE/swampy_align.sh ${file_path}/${file_prefix}_reads.fastq ${file_path}/NC_045512v2.fa ${file_prefix} ${file_path}
-#wbe sam2PB -v ${file_prefix} -f NC_045512v2.fa -s ${file_prefix}_alignment.sam -o ${file_path}
 #
-##DETECTING PEAKS
-#wbe detectPeaks -T 56 -i ${MAT} -r ${REF_MAT} -v ${file_prefix} -f NC_045512v2.fa -o ${file_path}
-### gdb --args wbe detectPeaks -T 32 -i ${MAT} -v ${file_prefix} -f NC_045512v2.fa -o ${file_path}
+### Prepare Files
+##mv ${file_path}/${file_prefix}_alignment.sam ${file_path}/${file_prefix}_alignment.sam_orig
+##python select_subset_reads.py ${file_path}/${file_prefix}_alignment.sam_orig ${file_path}/${file_prefix}_alignment.sam
+##
+##cp ${file_path}/${file_prefix}_alignment.sam_orig ../Freyja/${file_prefix}_alignment.sam
+#####cp ${file_path}/resorted.bam ../Freyja/
+##cd ../Freyja
+##source run.sh
+##cd -
+#
+#wbe sam2PB -v ${file_prefix} -f NC_045512v2.fa -s ${file_prefix}_alignment.sam -o ${file_path}
+
+#DETECTING PEAKS
+wbe detectPeaks -T 56 -i ${MAT} -v ${file_prefix} -f NC_045512v2.fa -o ${file_path}
+##wbe detectPeaks -T 56 -i ${MAT} -r ${REF_MAT} -v ${file_prefix} -f NC_045512v2.fa -o ${file_path}
+#### gdb --args wbe detectPeaks -T 32 -i ${MAT} -v ${file_prefix} -f NC_045512v2.fa -o ${file_path}
 
 #CALCULATING MUTATION DISTANCE
 # wbe refinePeaks -T 32 -i ${MAT} -v ${file_prefix} -f NC_045512v2.fa -o ${file_path}
