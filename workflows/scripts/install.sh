ceedb17d3525049e90ec98f1d2042c1dbdd70d94
enigmurl <43832426+enigmurl@users.noreply.github.com>
2025-01-12 23:20:33 -0800

    Add workflows

---
+++ workflows/scripts/install.sh
@@ -0,0 +1,10 @@
+rm -rf build
+mkdir build
+cd build
+git clone https://github.com/microsoft/vcpkg.git
+./vcpkg/bootstrap-vcpkg.sh
+./vcpkg/vcpkg install jsoncpp
+wget https://github.com/oneapi-src/oneTBB/archive/2019_U9.tar.gz
+tar -xvzf 2019_U9.tar.gz
+cmake -DTBB_DIR=${PWD}/oneTBB-2019_U9 -DCMAKE_PREFIX_PATH=${PWD}/oneTBB-2019_U9/cmake -DCMAKE_TOOLCHAIN_FILE=${PWD}/vcpkg/scripts/buildsystems/vcpkg.cmake ..
+# cmake -DCMAKE_BUILD_TYPE=Debug -DTBB_DIR=${PWD}/oneTBB-2019_U9 -DCMAKE_PREFIX_PATH=${PWD}/oneTBB-2019_U9/cmake -DCMAKE_TOOLCHAIN_FILE=${PWD}/vcpkg/scripts/buildsystems/vcpkg.cmake ..
