331f9d93a678c3336aa136149ad84fcdcb83fa0f
enigmurl <43832426+enigmurl@users.noreply.github.com>
2025-03-24 11:09:17 -0700

    Set default build type to release

--- workflow/scripts/install.sh
+++ workflow/scripts/install.sh
@@ -6,5 +6,5 @@ git clone https://github.com/microsoft/vcpkg.git
 ./vcpkg/vcpkg install jsoncpp
 wget https://github.com/oneapi-src/oneTBB/archive/2019_U9.tar.gz
 tar -xvzf 2019_U9.tar.gz
-cmake -DTBB_DIR=${PWD}/oneTBB-2019_U9 -DCMAKE_PREFIX_PATH=${PWD}/oneTBB-2019_U9/cmake -DCMAKE_TOOLCHAIN_FILE=${PWD}/vcpkg/scripts/buildsystems/vcpkg.cmake ..
+cmake -DCMAKE_BUILD_TYPE=Release -DTBB_DIR=${PWD}/oneTBB-2019_U9 -DCMAKE_PREFIX_PATH=${PWD}/oneTBB-2019_U9/cmake -DCMAKE_TOOLCHAIN_FILE=${PWD}/vcpkg/scripts/buildsystems/vcpkg.cmake ..
 # cmake -DCMAKE_BUILD_TYPE=Debug -DTBB_DIR=${PWD}/oneTBB-2019_U9 -DCMAKE_PREFIX_PATH=${PWD}/oneTBB-2019_U9/cmake -DCMAKE_TOOLCHAIN_FILE=${PWD}/vcpkg/scripts/buildsystems/vcpkg.cmake ..
